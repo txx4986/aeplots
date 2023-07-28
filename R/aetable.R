4a547d95c71776deb66c333fc9ad61432dea917e
txx4986 <txx4986@gmail.com>
2023-07-28 12:42:04 +0100

    increased number of arms for aetable

--- R/aetable.R
+++ R/aetable.R
@@ -6,8 +6,14 @@
 #' @param arm Name of arm column
 #' @param date_rand Name of date_rand column
 #' @param last_visit Name of last_visit column
-#' @param intervention_name Name of intervention
+#' @param control Factor level of control arm
+#' @param intervention1 Factor level of intervention 1 arm
+#' @param intervention2 Factor level of intervention 2 arm
+#' @param intervention3 Factor level of intervention 3 arm
 #' @param control_name Name of control
+#' @param intervention1_name Name of intervention 1
+#' @param intervention2_name Name of intervention 2
+#' @param intervention3_name Name of intervention 3
 #'
 #' @return Flextable of AE summary by body system class
 #'
@@ -22,10 +28,23 @@
 #' @examples
 #' aetable(df, body_system_class="ae_02", id="participant_id", arm="treatment_arm", date_rand="randomisation_date", last_visit="date_of_last_visit")
 aetable <- function(data, body_system_class = "body_system_class", id = "id", arm = "arm", date_rand = "date_rand",
-                    last_visit = "last_visit", intervention_name="Intervention", control_name="Control"){
+                    last_visit = "last_visit", control = "C", intervention1 = "I1", intervention2 = "I2",
+                    intervention3 = "I3", control_name="Control", intervention1_name="Intervention 1",
+                    intervention2_name = "Intervention 2", intervention3_name = "Intervention 3"){
   # change the column names
   dataset <- data %>%
-    rename("body_system_class" = body_system_class, "id" = id, "arm" = arm, "date_rand" = date_rand, "last_visit" = last_visit)
+    rename("body_system_class" = body_system_class, "id" = id, "arm" = arm, "date_rand" = date_rand,
+           "last_visit" = last_visit)
+
+  arm_number <- length(levels(arm))
+  # recode arm factor
+  dataset <- dataset %>%
+    mutate(
+      arm =
+        case_when(arm_number==2 ~ recode_factor(arm, control="C", intervention1="I1"),
+                  arm_number==3 ~ recode_factor(arm, control="C", intervention1="I1", intervention2="I2"),
+                  arm_number==4 ~ recode_factor(arm, control="C", intervention1="I1", intervention2="I2",
+                                                intervention3="I3")))
 
   # checks if the variable type for each column is correct
   stopifnot("body_system_class variable type is not factor!" = is.factor(dataset[["body_system_class"]]))
@@ -35,62 +54,57 @@ aetable <- function(data, body_system_class = "body_system_class", id = "id", ar
   stopifnot("last_visit variable type is not Date!" = is.Date(dataset[["last_visit"]]))
 
   # number of participants at risk per arm
-  N1 <- length(unique((dataset %>% filter(arm=="I"))$id))
-  N2 <- length(unique((dataset %>% filter(arm=="C"))$id))
+  N0 <- length(unique((dataset %>% filter(arm=="C"))$id))
+  N1 <- length(unique((dataset %>% filter(arm=="I1"))$id))
+  if (arm_number==3 | 4){
+    N2 <- length(unique((dataset %>% filter(arm=="I2"))$id))
+  }
+  if (arm_number==4){
+    N3 <- length(unique((dataset %>% filter(arm=="I3"))$id))
+  }
 
   Table1 <- dataset %>%
     # follow up time is computed as difference between randomisation date and last visit date (units=weeks)
     mutate(follow_up_time = as.numeric(difftime(last_visit, date_rand, units="weeks"))) %>%
-    group_by(body_system_class, id, arm, follow_up_time) %>%
+    group_by(body_system_class, id, arm) %>%
     # count total number of adverse events of each participant for each body system class
-    count() %>%
-    group_by(body_system_class) %>%
     summarise(
-      # number of participants with at least one adverse event for each body system class (intervention arm)
-      Frequency_Intervention = length(unique(id[arm=="I"])),
-      # total number of adverse events for each body system class (intervention arm)
-      Events_Intervention = sum(n[arm=="I"]),
-      # total length of follow up time for each body system class (intervention arm)
-      Total_Time_Intervention = sum(follow_up_time[arm=="I"]),
-      # mean number of adverse events for each participant for each body system class (intervention arm)
-      Mean_Intervention = round(mean(n[arm=="I"]), 1),
-      # standard deviation of number of adverse events for each participant for each body system class (intervention arm)
-      SD_Intervention = round(sd(n[arm=="I"]), 1),
-      # number of participants with at least one adverse event for each body system class (control arm)
-      Frequency_Control = length(unique(id[arm=="C"])),
-      # total number of adverse events for each body system class (control arm)
-      Events_Control = sum(n[arm=="C"]),
-      # total length of follow up time for each body system class (control arm)
-      Total_Time_Control= sum(follow_up_time[arm=="C"]),
-      # mean number of adverse events for each participant for each body system class (control arm)
-      Mean_Control = round(mean(n[arm=="C"]), 1),
-      # standard deviation of number of adverse events for each participant for each body system class (control arm)
-      SD_Control = round(sd(n[arm=="C"]), 1)) %>%
+      sum_follow_up_time = sum(follow_up_time),
+      n = n()) %>%
+    group_by(body_system_class, arm) %>%
+    summarise(
+      # number of participants with at least one adverse event for each body system class and arm
+      Frequency = length(unique(id)),
+      # total number of adverse events for each body system class and arm
+      Events = sum(n),
+      # total length of follow up time for each body system class and arm
+      Total_Time = sum(sum_follow_up_time),
+      # mean number of adverse events for each participant for each body system class and arm
+      Mean = round(mean(n), 1),
+      # standard deviation of number of adverse events for each participant for each body system class and arm
+      SD = round(sd(n), 1)) %>%
     mutate(
-      # IR = Events_Intervention / Total_Time_Intervention (intervention arm)
-      IR_Intervention = scales::percent(Events_Intervention / Total_Time_Intervention, 0.1),
-      # Proportions = Frequency_Intervention / N1 (intervention arm)
-      Proportions_Intervention = scales::percent(Frequency_Intervention / N1, 0.1),
-      # IR = Events_Control / Total_Time_Control (control arm)
-      IR_Control = scales::percent(Events_Control / Total_Time_Control, 0.1),
-      # Proportions = Frequency_Control / N2 (control arm)
-      Proportions_Control = scales::percent(Frequency_Control / N2, 0.1)) %>%
+      # IR = Events / Total_Time
+      IR = scales::percent(Events / Total_Time, 0.1),
+      # Proportions = Frequency / N
+      Proportions =
+        case_when(arm=="C" ~ scales::percent(Frequency / N0, 0.1),
+                  arm=="I1" ~ scales::percent(Frequency / N1, 0.1),
+                  arm=="I2" ~ scales::percent(Frequency / N2, 0.1),
+                  arm=="I3" ~ scaless::percent(Frequency) / N3, 0.1)) %>%
     mutate(
       # one decimal place for mean and SD
-      across(c(Mean_Intervention, SD_Intervention, Mean_Control, SD_Control), \(x) format(x, nsmall=1))) %>%
+      across(c(Mean, SD), \(x) format(x, nsmall=1))) %>%
     mutate(
       # combine Frequency & Proportions, Events & IR, Mean & SD columns
-      Frequency_Intervention = str_glue("{Frequency_Intervention} ({Proportions_Intervention})"),
-      Events_Intervention = str_glue("{Events_Intervention} ({IR_Intervention})"),
-      Mean_Intervention = str_glue("{Mean_Intervention} ({SD_Intervention})"),
-      Frequency_Control = str_glue("{Frequency_Control} ({Proportions_Control})"),
-      Events_Control = str_glue("{Events_Control} ({IR_Control})"),
-      Mean_Control = str_glue("{Mean_Control} ({SD_Control})")) %>%
+      Frequency = str_glue("{Frequency} ({Proportions})"),
+      Events = str_glue("{Events} ({IR})"),
+      Mean = str_glue("{Mean} ({SD})")) %>%
     select(
       # drop Total_Time, SD, IR and Proportions from table
-      -c(Total_Time_Intervention, SD_Intervention, Total_Time_Control, SD_Control, IR_Intervention,
-         Proportions_Intervention, IR_Control, Proportions_Control)
-    )
+      -c(Total_Time, SD, IR, Proportions)) %>%
+    pivot_wider(
+      names_from = arm, values_from = c(Frequency, Events, Mean))
 
   # to produce nice table
   name1 <- "N" %p% subsc("1")
@@ -98,21 +112,22 @@ aetable <- function(data, body_system_class = "body_system_class", id = "id", ar
   border <- fp_border_default(width=1.5)
 
   Table1_print <- Table1 %>%
+    relocate(Events_I1, Mean_I1, .after = Frequency_I1) %>%
     flextable() %>%
     add_header(
-      values = c(Frequency_Intervention="At least one event",
-                 Events_Intervention="Number of events",
-                 Mean_Intervention="Number of events",
-                 Frequency_Control="At least one event",
-                 Events_Control="Number of events",
-                 Mean_Control="Number of events")) %>%
+      values = c(Frequency_I1="At least one event",
+                 Events_I1="Number of events",
+                 Mean_I1="Number of events",
+                 Frequency_C="At least one event",
+                 Events_C="Number of events",
+                 Mean_C="Number of events")) %>%
     add_header_row(
-      values=c("", str_glue("{intervention_name} ({name1}={N1})"), str_glue("{control_name} ({name2}={N2})")),
+      values=c("", str_glue("{intervention1_name} ({name1}={N1})"), str_glue("{control_name} ({name2}={N0})")),
       colwidths = c(1, 3, 3)) %>%
     set_header_labels(
-      body_system_class="Body system class", Frequency_Intervention="N (%)", Events_Intervention="n (IR)",
-      Mean_Intervention="Mean number of events per participant (SD)", Frequency_Control="N (%)",
-      Events_Control="n (IR)", Mean_Control="Mean number of events per participant (SD)") %>%
+      body_system_class="Body system class", Frequency_I1="N (%)", Events_I1="n (IR)",
+      Mean_I1="Mean number of events per participant (SD)", Frequency_C="N (%)",Events_C="n (IR)",
+      Mean_C="Mean number of events per participant (SD)") %>%
     merge_h(part="header") %>%
     flextable::align(align="center", j = c(2:7), part="all") %>%
     autofit() %>%
@@ -125,6 +140,5 @@ aetable <- function(data, body_system_class = "body_system_class", id = "id", ar
     bg(part="header", bg="gray80") %>%
     bg(part="body", bg="white")
 
-  return(Table1)
-  print(plot(Table1_print))
+  plot(Table1_print)
 }
