b2dfc828c594f8ae7117b2c2a5c7971d26cd6166
txx4986 <txx4986@gmail.com>
2023-08-07 11:32:19 +0100

    uploaded sample datasets

---
+++ R/data.R
@@ -0,0 +1,43 @@
+#' @title Sample adverse event dataset with 2 treatment arms
+#'
+#' @format A data frame with 370 rows and 9 variables:
+#' \describe{
+#'    \item{ae_01}{Adverse event}
+#'    \item{ae_02}{Body system class}
+#'    \item{id}{Participant's id}
+#'    \item{arm}{Treatment arm}
+#'    \item{date_rand}{Date of randomisation}
+#'    \item{last_visit}{Date of last visit}
+#'    \item{IgEstrat}{Variable 1 to be included in Poisson regression}
+#'    \item{agestrat}{Variable 2 to be included in Poisson regression}
+#'    \item{ae_05}{Severity}
+#' }
+"df2"
+
+#' @title Sample adverse event dataset with 3 treatment arms
+#'
+#' @format A data frame with 370 rows and 6 variables:
+#' \describe{
+#'    \item{ae_01}{Adverse event}
+#'    \item{ae_02}{Body system class}
+#'    \item{id}{Participant's id}
+#'    \item{arm}{Treatment arm}
+#'    \item{date_rand}{Date of randomisation}
+#'    \item{last_visit}{Date of last visit}
+#'    \item{ae_05}{Severity}
+#' }
+"df3"
+
+#' @title Sample adverse event dataset with 4 treatment arms
+#'
+#' @format A data frame with 370 rows and 6 variables:
+#' \describe{
+#'    \item{ae_01}{Adverse event}
+#'    \item{ae_02}{Body system class}
+#'    \item{id}{Participant's id}
+#'    \item{arm}{Treatment arm}
+#'    \item{date_rand}{Date of randomisation}
+#'    \item{last_visit}{Date of last visit}
+#'    \item{ae_05}{Severity}
+#' }
+"df4"
