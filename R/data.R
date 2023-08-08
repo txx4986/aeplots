63f791b02f05b994dc86882f69dedf9368f7b2c6
txx4986 <txx4986@gmail.com>
2023-08-08 14:49:33 +0100

    updated new sample datasets

--- R/data.R
+++ R/data.R
@@ -2,42 +2,42 @@
 #'
 #' @format A data frame with 370 rows and 9 variables:
 #' \describe{
-#'    \item{ae_01}{Adverse event}
-#'    \item{ae_02}{Body system class}
 #'    \item{id}{Participant's id}
-#'    \item{arm}{Treatment arm}
+#'    \item{arm}{Treatment arm: Placebo & Intervention}
+#'    \item{ae_pt}{Adverse event preferred term}
+#'    \item{aebodysys}{Body system class}
+#'    \item{severity}{Severity}
 #'    \item{date_rand}{Date of randomisation}
 #'    \item{last_visit}{Date of last visit}
-#'    \item{IgEstrat}{Variable 1 to be included in Poisson regression}
-#'    \item{agestrat}{Variable 2 to be included in Poisson regression}
-#'    \item{ae_05}{Severity}
+#'    \item{variable1}{Variable 1 to be included in Poisson regression}
+#'    \item{variable2}{Variable 2 to be included in Poisson regression}
 #' }
 "df2"
 
 #' @title Sample adverse event dataset with 3 treatment arms
 #'
-#' @format A data frame with 370 rows and 6 variables:
+#' @format A data frame with 370 rows and 7 variables:
 #' \describe{
-#'    \item{ae_01}{Adverse event}
-#'    \item{ae_02}{Body system class}
 #'    \item{id}{Participant's id}
-#'    \item{arm}{Treatment arm}
+#'    \item{arm}{Treatment arm: Placebo, Intervention 1, Intervention 2}
+#'    \item{ae_pt}{Adverse event preferred term}
+#'    \item{aebodysys}{Body system class}
+#'    \item{severity}{Severity}
 #'    \item{date_rand}{Date of randomisation}
 #'    \item{last_visit}{Date of last visit}
-#'    \item{ae_05}{Severity}
 #' }
 "df3"
 
 #' @title Sample adverse event dataset with 4 treatment arms
 #'
-#' @format A data frame with 370 rows and 6 variables:
+#' @format A data frame with 370 rows and 7 variables:
 #' \describe{
-#'    \item{ae_01}{Adverse event}
-#'    \item{ae_02}{Body system class}
 #'    \item{id}{Participant's id}
-#'    \item{arm}{Treatment arm}
+#'    \item{arm}{Treatment arm: Placebo, Intervention 1, Intervention 2, Intervention 3}
+#'    \item{ae_pt}{Adverse event preferred term}
+#'    \item{aebodysys}{Body system class}
+#'    \item{severity}{Severity}
 #'    \item{date_rand}{Date of randomisation}
 #'    \item{last_visit}{Date of last visit}
-#'    \item{ae_05}{Severity}
 #' }
 "df4"
